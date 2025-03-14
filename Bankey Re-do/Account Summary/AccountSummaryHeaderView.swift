2ad6d5345f66e09bc30ba32957e286fb9ea22421
Todd Smith <ironsmith@me.com>
2025-03-14 08:13:37 -0700

    feat: Style Header View

---
+++ Bankey Re-do/Account Summary/AccountSummaryHeaderView.swift
@@ -0,0 +1,49 @@
+//
+//  AccountSummaryHeaderView.swift
+//  Bankey Re-do
+//
+//  Created by Todd Smith on 3/14/25.
+//
+
+import UIKit
+
+class AccountSummaryHeaderView: UIView {
+    
+    @IBOutlet var contentView: UIView!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+    }
+    
+    override var intrinsicContentSize: CGSize {
+        return CGSize(width: UIView.noIntrinsicMetric, height: 144)
+    }
+    
+    private func commonInit() {
+        
+        let bundle = Bundle(for: AccountSummaryHeaderView.self)
+        bundle.loadNibNamed("AccountSummaryHeaderView", owner: self, options: nil)
+        addSubview(contentView)
+        contentView.backgroundColor = appColor
+        
+        contentView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            contentView.topAnchor.constraint(equalTo: topAnchor),
+            contentView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            contentView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            contentView.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ])
+        
+    }
+    
+    
+    
+    
+}
--- Bankey Re-do/Account Summary/AccountSummaryViewController.swift
+++ Bankey Re-do/Account Summary/AccountSummaryViewController.swift
@@ -29,6 +29,7 @@ extension AccountSummaryViewController {
     private func setup() {
         
         setupTableView()
+        setupTableHeaderView()
     }
     
     private func setupTableView() {
@@ -47,6 +48,17 @@ extension AccountSummaryViewController {
 //            tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
 //            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
 //        ])
+    }
+    
+    private func setupTableHeaderView() {
+        let header = AccountSummaryHeaderView(frame: .zero)
+        
+        var size = header.systemLayoutSizeFitting(UIView.layoutFittingCompressedSize)
+        size.width = UIScreen.main.bounds.width
+        header.frame.size = size
+        
+        tableView.tableHeaderView = header
+        
     }
 }
 

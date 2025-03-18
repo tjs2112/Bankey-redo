9374e0020e6bc28cb555af6e5d1319f76ae90faf
Todd Smith <ironsmith@me.com>
2025-03-17 22:09:40 -0700

    starting to rename project

---
+++ Bankey Re-doUnitTests/CurrencyFormatterTests.swift
@@ -0,0 +1,23 @@
+//
+//  CurrencyFormatterTests.swift
+//  Bankey Re-do
+//
+//  Created by Todd Smith on 3/17/25.
+//
+
+import XCTest
+
+@testable import "Bankey Re-do"
+
+class Test: XCTestCase {
+   
+    
+    override func setUp() {
+        super.setUp()
+
+    }
+    
+    func testShouldBeVisible() throws {
+    
+    }
+}
